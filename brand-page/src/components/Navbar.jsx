202241e5c02b6e0225d50030b5109b2002cc65c0
Samyak Jain <samyak.9109@gmail.com>
2026-05-16 15:53:15 +0530

    brand page , a simp,e react pracitce project

---
+++ brand-page/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import logo from "../assets/logo.png"
+const Navbar = () => {
+  return (
+    <nav className="container">
+      <div className="logo">
+        <img src={logo} alt="logo" />
+      </div>
+      <ul>
+        <li href="#">Menu</li>
+        <li href="#">Location</li>
+        <li href="#">About</li>
+        <li href="#">Contact</li>
+      </ul>
+
+      <button>login</button>
+    </nav>
+  );
+}
+
+export default Navbar
